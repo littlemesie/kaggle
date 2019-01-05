61d0af426aace48e8c96dcceef79961e2e584bef
littlemesie <1319417676@qq.com>
2019-01-06 00:32:17 +0800

    update

--- movie_sentiment_analysis/model.py
+++ movie_sentiment_analysis/model.py
@@ -15,39 +15,48 @@ from sklearn.feature_extraction.text import TfidfVectorizer as TFIDF
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB as MNB
+from gensim.models import Word2Vec
+from movie_sentiment_analysis.feature import en_word2vec_model
 
 from movie_sentiment_analysis import feature
 
 def logistic(train, test):
     """逻辑回归"""
-    tokenizer = TweetTokenizer()
-    vectorizer = TFIDF(ngram_range=(1, 2), tokenizer=tokenizer.tokenize)
-    full_text = list(train['Phrase'].values) + list(test['Phrase'].values)
-    vectorizer.fit(full_text)
-    train_vectorized = vectorizer.transform(train['Phrase'])
-    test_vectorized = vectorizer.transform(test['Phrase'])
+    train_x, test_x = tf_idf(train, test)
     y = train['Sentiment']
     logreg = LR()
     ovr = OneVsRestClassifier(logreg)
 
-    ovr.fit(train_vectorized, y)
-    scores = cross_val_score(ovr, train_vectorized, y, scoring='accuracy', n_jobs=-1, cv=3)
-    print('Cross-validation mean accuracy {0:.2f}%, std {1:.2f}.'.format(np.mean(scores) * 100, np.std(scores) * 100))
+    ovr.fit(train_x, y)
+    scores = cross_val_score(ovr, train_x, y, scoring='accuracy', n_jobs=-1, cv=3)
+    print('Cross-validation mean accuracy {0:.2f}%'.format(np.mean(scores) * 100))
 
-
-def random_forest(train, test):
-    """随机森林"""
+def naive_bayes(train, test):
+    """贝叶斯"""
     train_x, test_x = tf_idf(train, test)
-    forest = RandomForestClassifier(n_estimators=100, n_jobs=2)
-    label = train['Sentiment']
-    forest = forest.fit(train_x, label)
-    print('Score: ', forest.score(train_x, label))
-    # 测试集
-    result = forest.predict(test_x)
+    y = train['Sentiment']
+    model_NB = MNB()
+    scores = np.mean(cross_val_score(model_NB, train_x, y, cv=3, n_jobs=2, scoring='accuracy'))
+    print('Cross-validation mean accuracy {0:.2f}%'.format(scores * 100))
 
-    print('保存结果...')
-    submission_df = pd.DataFrame(data={'PhraseId': test['PhraseId'], 'Sentiment': result})
-    print(submission_df.head(10))
+
+def random_forest_word2vec(train, test):
+    """随机森林+word2vec"""
+    train_x = train['Phrase']
+    test_x = test['Phrase']
+    y = train['Sentiment']
+    path = "../data/word2vec-nlp"
+    model_name = "%s/%s" % (path, "en_word2vec_model")
+    model = Word2Vec.load(model_name)
+    train_data_vecs = feature.get_avg_feature_vecs(train_x, model, 300)
+    test_data_vecs = feature.get_avg_feature_vecs(test_x, model, 300)
+    forest = RandomForestClassifier(n_estimators=100, n_jobs=2)
+    scores = np.mean(cross_val_score(forest, train_data_vecs, y, cv=3, n_jobs=2, scoring='accuracy'))
+    print("Cross-validation mean accuracy {0:.2f}% ".format(scores * 100))
+
+    # 测试集
+    result = forest.predict(test_data_vecs)
 
 def tf_idf(train_data, test_data):
     len_train = len(train_data)
@@ -64,4 +73,5 @@ def tf_idf(train_data, test_data):
 if __name__ == '__main__':
     train, test = feature.get_data()
     # logistic(train, test)
-    random_forest(train, test)
+    # naive_bayes(train, test)
+    random_forest_word2vec(train, test)
