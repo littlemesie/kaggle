be2f6f3cb875260dbf28e0449e2d7b1333a3be0d
littlemesie <littlemesie@163.com>
2019-03-28 19:59:12 +0800

    update cnn

--- src/pytorch/cnn_digit_recognizer.py
+++ src/pytorch/cnn_digit_recognizer.py
@@ -37,6 +37,7 @@ class CustomedDataSet(Dataset):
 
 
 class CNN(nn.Module):
+    """创建CNN模型"""
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Sequential(  # input shape (1, 28, 28)
@@ -70,18 +71,19 @@ class CNN(nn.Module):
         return output, x  # return x for visualization
 
 
-def load_data_train():
-    # 先转换成 torch 能识别的 Dataset
+def load_data_train(train_tatio = 0.8):
+    """数据加载"""
     pd_train = pd.read_csv('../../data/digit-recognizer/train.csv', header=0)
-
-    # 找到数据分割点
+    # 切分为训练集和测试集
     row = pd_train.shape[0]
-    split_num = int(TRAIN_TATIO*row)
-
+    split_num = int(train_tatio * row)
     pd_data_train = pd_train[:split_num]
     pd_data_test = pd_train[split_num:]
+    # 转换成 torch 能识别的 Dataset
     data_train = CustomedDataSet(pd_data_train, data_type=True)
     data_test = CustomedDataSet(pd_data_test, data_type=True)
+
+
 
     # 数据读取器 (Data Loader)
     # Data Loader for easy mini-batch return in training, the image batch shape will be (50, 1, 28, 28)
@@ -97,20 +99,10 @@ def load_data_pre():
     return loader_pre
 
 
-def optimizer_lossfunction(cnn):
-    '''
-    3. 设置优化器和损失函数
-    '''
-    # optimizer 优化器 是训练的工具，lr表示学习率
-    '''
-    lr
-    >>> # lr = 0.05     if epoch < 30
-    >>> # lr = 0.005    if 30 <= epoch < 80
-    >>> # lr = 0.0005   if epoch >= 80
-    Adam 好像没这个参数 -> momentum 动量参数： 计算速度更新，优化收敛速度
-    '''
-    optimizer = torch.optim.Adam(cnn.parameters(), lr=LR, betas=(0.9, 0.99))  # optimize all cnn parameters
-    loss_func = nn.CrossEntropyLoss()  # the target label is not one-hotted
+def optimizer_lossfunction(cnn,lr=0.0001):
+    """设置优化器和损失函数"""
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=lr, betas=(0.9, 0.99))
+    loss_func = nn.CrossEntropyLoss()
     return optimizer, loss_func
 
 
@@ -146,55 +138,64 @@ def plot_with_labels(lowDWeights, labels):
 
 
 def train_model(cnn, optimizer, loss_func, loader_train, loader_test):
-    plt.ion()
-    # Hyper Parameters
-    EPOCH = 10  # train the training data n times, to save time, we just train 1 epoch
+    """训练模型"""
+    # plt.ion()
+    EPOCH = 10
     # training and testing
     for epoch in range(EPOCH):
         num = 0
-        # gives batch data, normalize x when iterate train_loader
         for step, (x, y) in enumerate(loader_train):
-            b_x = Variable(x)  # batch x
-            b_y = Variable(y)  # batch y
-
-            output = cnn(b_x)[0]  # cnn output
-            loss = loss_func(output, b_y)  # cross entropy loss
-            optimizer.zero_grad()  # clear gradients for this training step
-            loss.backward()  # backpropagation, compute gradients
-            optimizer.step()  # apply gradients
-
-            # print('-'*30, step)
+            b_x = Variable(x)
+            b_y = Variable(y)
+            output = cnn(b_x)[0]
+            loss = loss_func(output, b_y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
             if step % 50 == 0:
                 num += 1
                 for _, (x_t, y_test) in enumerate(loader_test):
-                    x_test = Variable(x_t)  # batch x
+                    test_sum = y_test.size(0)
+                    accuracy_sum = 0
+                    x_test = Variable(x_t)
                     test_output, last_layer = cnn(x_test)
                     pred_y = torch.max(test_output, 1)[1].data.squeeze()
-                    accuracy = sum(pred_y == y_test) / float(y_test.size(0))
-                    print('Epoch:', epoch, '| Num: ',  num, '| Step: ',  step, '| train loss: %.4f' % loss.data, '| test accuracy: %.4f' % accuracy)
+
+                    for i in range(test_sum):
+                        if pred_y[i] == y_test[i]:
+                            # print('预测:',pred_y[i], '正确:', y_test[i])
+                            accuracy_sum += 1
+                    accuracy = accuracy_sum / test_sum
+                    print('Epoch:', epoch, '| Num: ',  num, '| Step: ', step, '| train loss: %.4f' % loss.data, '| test accuracy: %.4f' % accuracy)
+            # if step % 50 == 0:
+            #     num += 1
+            #     for _, (x_t, y_test) in enumerate(loader_test):
+            #         x_test = Variable(x_t)
+            #         test_output, last_layer = cnn(x_test)
+            #         pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            #         accuracy = sum(pred_y == y_test) / float(y_test.size(0))
+            #         print('Epoch:', epoch, '| Num: ',  num, '| Step: ',  step, '| train loss: %.4f' % loss.data, '| test accuracy: %.4f' % accuracy)
                     # 可视化展现
-                    show(last_layer, y_test)
-    plt.ioff()
+                    # show(last_layer, y_test)
+    # plt.ioff()
     return cnn
 
 
-def model_train():
+def model_cnn():
+    """模型训练"""
     # 1.加载数据
-    loader_train, loader_test = load_data_train()
-
+    loader_train, loader_test = load_data_train(0.8)
     # 2.创建CNN模型
     cnn = CNN()
-
     # 3. 设置优化器和损失函数
-    optimizer, loss_func = optimizer_lossfunction(cnn)
-
+    optimizer, loss_func = optimizer_lossfunction(cnn, 0.0001)
     # 4. 训练模型
     cnn = train_model(cnn, optimizer, loss_func, loader_train, loader_test)
     return cnn
 
 
 def prediction(cnn, loader_pre):
-    # print 10 predictions from test data
+    """预测"""
     for step, (x, y) in enumerate(loader_pre):
         b_x = Variable(x)  # batch x
         test_output, _ = cnn(b_x)
@@ -208,15 +209,14 @@ def prediction(cnn, loader_pre):
 if __name__ == "__main__":
     global BATCH_SIZE, TRAIN_TATIO, LR, momentum
     BATCH_SIZE = 50
-    TRAIN_TATIO = 0.8
-    LR = 0.0001
 
     # 训练模型
     file_path = '../../data/digit-recognizer/net.pkl'
-    cnn = model_train()
+    cnn = model_cnn()
     torch.save(cnn, file_path)
 
-    # # 预测数据
-    # loader_pre = load_data_pre()
-    # cnn = torch.load(file_path)
-    # pre_data = prediction(cnn)
+    # 预测数据
+    loader_pre = load_data_pre()
+    cnn = torch.load(file_path)
+    pre_data = prediction(cnn, loader_pre)
+    print(pre_data)
